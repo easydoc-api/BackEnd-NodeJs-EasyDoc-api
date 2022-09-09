9e66647a01691f1de84e22975537e40bb328fe67
raqueldesa <silva.raquel.de.sa@gmail.com>
2022-09-09 17:16:59 -0400

    feat: get (all) labExams

--- src/routes/examesLaboratorial/index.ts
+++ src/routes/examesLaboratorial/index.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { examLabCreateController } from "../../controllers/examesLaboratoriais/examLabCreate.controller";
+import { examsLabListController } from "../../controllers/examesLaboratoriais/examsLabList.controller";
 
 const labExams = Router();
 
 export const labExamsRouter = () => {
   labExams.post("/register", examLabCreateController);
-  labExams.get("");
+  labExams.get("", examsLabListController);
   labExams.get("/paciente/:id");
   labExams.patch("/:id");
   labExams.delete("/:id");
