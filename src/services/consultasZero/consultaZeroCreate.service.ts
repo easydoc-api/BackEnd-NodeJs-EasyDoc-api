e360d0f4b0e6d28ae45b52d54aa7ba30ce62eeb1
arc4dex <pedroiago.bernardes@gmail.com>
2022-09-09 11:39:15 -0300

    feat: consultaZero service

---
+++ src/services/consultasZero/consultaZeroCreate.service.ts
@@ -0,0 +1,30 @@
+import AppDataSource from "../../data-source";
+import { ConsultaZero } from "../../entities/consultaZero.entity";
+import { AppError } from "../../errors/AppError";
+import { IConsultZeroRequest } from "../../interfaces/consultasZero"
+
+const appointmentZeroCreateService = async ({paridade, consanguinidade, idadeGestacional, dataMenstruacao, primeiroUltrassom, semanaGestacional, diaGestacional, historiaPregressa, historiaGinecologicaObstetrica}: IConsultZeroRequest) => {
+
+  const appointmentZeroRepository = AppDataSource.getRepository(ConsultaZero)
+
+  const zeroAppointment = await appointmentZeroRepository.find()
+
+ 
+  const appointmentZero = appointmentZeroRepository.create({
+    paridade,
+    consanguinidade,
+    idadeGestacional,
+    dataMenstruacao,
+    primeiroUltrassom,
+    semanaGestacional,
+    diaGestacional,
+    historiaPregressa,
+    historiaGinecologicaObstetrica
+  })
+
+  await appointmentZeroRepository.save(appointmentZero)
+
+  return
+
+
+}
