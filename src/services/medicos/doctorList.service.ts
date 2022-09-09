2f40f03676dfa81cdcc957da7a5f1a111fcad7cd
Cayo Nakasato <cayonakasato112@gmail.com>
2022-09-09 13:43:57 -0400

    feat: can list one and all employees

---
+++ src/services/medicos/doctorList.service.ts
@@ -0,0 +1,10 @@
+import AppDataSource from "../../data-source"
+import { Medico } from "../../entities/medico.entity"
+
+export const doctorListService = () =>{
+    const doctorRepository = AppDataSource.getRepository(Medico)
+
+    const employees = doctorRepository.find()
+
+    return employees
+}
---
+++ src/services/medicos/doctorListById.service.ts
@@ -0,0 +1,20 @@
+import AppDataSource from "../../data-source";
+import { Medico } from "../../entities/medico.entity";
+import { AppError } from "../../errors/AppError";
+
+export const doctorListOneByIdService = async (id: string) => {
+    console.log(id)
+  const doctorRepository = AppDataSource.getRepository(Medico);
+
+  const selectedDoctor = await doctorRepository.findOne({
+    where: {
+      id
+    },
+  });
+
+  if (!selectedDoctor) {
+    throw new AppError("Doctor not found!", 404);
+  }
+
+  return selectedDoctor;
+};
