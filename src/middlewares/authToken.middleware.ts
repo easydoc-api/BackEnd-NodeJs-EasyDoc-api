3d566e5ad5c173905a5765345a7a80d9ca033a7a
Cayo Nakasato <cayonakasato112@gmail.com>
2022-09-09 15:33:11 -0400

    feat: adm validation and adding to the routes

--- src/middlewares/authToken.middleware.ts
+++ src/middlewares/authToken.middleware.ts
@@ -1,8 +1,34 @@
 import { Request, Response, NextFunction } from "express"
 import { AppError } from "../errors/AppError"
+import jwt from "jsonwebtoken"
 
 export const authTokenMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {}
+) => {
+  let token = req.headers.authorization
+
+  if (!token) {
+    throw new AppError("Token inválido!", 401)
+  }
+
+  token = token?.split(" ")[1]
+
+  jwt.verify(
+    token,
+    process.env.SECRET_KEY as string,
+    (error: any, decoded: any) => {
+      if (error) {
+        throw new AppError("Token inválido", 401)
+      }
+
+      req.user = {
+        adm: decoded.isAdm,
+        estaAtivo: decoded.isActive,
+        id: decoded.id,
+      }
+    }
+  )
+  next()
+}
