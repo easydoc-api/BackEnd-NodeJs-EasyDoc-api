a8cecbae81ec5fdf609d1dd8dee63cebd1dd3e92
Cayo Nakasato <cayonakasato112@gmail.com>
2022-09-09 15:18:24 -0400

    feat: can update a doctor

--- src/routes/medicos/index.ts
+++ src/routes/medicos/index.ts
@@ -4,6 +4,7 @@ import { doctorCreateController } from "../../controllers/medicos/doctorCreate.c
 import { doctorDeleteController } from "../../controllers/medicos/doctorDelete.controller"
 import { doctorListController } from "../../controllers/medicos/doctorList.controller"
 import { doctorListOneByIdController } from "../../controllers/medicos/doctorListById.controller"
+import { doctorUpdateController } from "../../controllers/medicos/doctorUpdate.controller"
 
 import { schemaValidationMiddleware } from "../../middlewares/schemaValidation.middleware"
 import { newDoctorSchema } from "../../schemas/newDoctor.schema"
@@ -11,10 +12,10 @@ import { newDoctorSchema } from "../../schemas/newDoctor.schema"
 const medic = Router()
 
 export const medicRoutes = () => {
-  medic.post("/register", schemaValidationMiddleware(newDoctorSchema), doctorCreateController) //Registro de Médicos
-  medic.get("", doctorListController) //Listagem de todos os Médicos
-  medic.get("/:id", doctorListOneByIdController) // Responsável por listar um medico especifico por meio do ID
-  medic.patch("/:id") // Responsável por alterar os dados de um médico especifico
-  medic.delete("/:id", doctorDeleteController) // Responsável por apagar todos os dados de um médico
+  medic.post("/register", schemaValidationMiddleware(newDoctorSchema), doctorCreateController) 
+  medic.get("", doctorListController) 
+  medic.get("/:id", doctorListOneByIdController) 
+  medic.patch("/:id", doctorUpdateController) 
+  medic.delete("/:id", doctorDeleteController) 
   return medic
 }
