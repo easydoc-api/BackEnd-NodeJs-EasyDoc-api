77deef01e28cb365fa987d57561defe506aa5a27
GabrielleMedeirosOliveira <gabriellemedeiros51@gmail.com>
2022-09-09 12:13:18 -0300

    Creating the Patient CRUD folder organization

--- src/routes/pacientes/index.ts
+++ src/routes/pacientes/index.ts
@@ -6,10 +6,10 @@ const patient = Router()
 
 export const patientRoutes = () => {
   patient.post("/register", schemaValidationMiddleware(newPatientSchema)) // autenticação
-  patient.get("") //autenticação e adm/r4/professor
-  patient.get("/:id") //autenticação
-  patient.patch("/:id") //autenticação
-  patient.get("/:id") //autenticação e adm
+  patient.get("") //autenticação e adm/r4/professor Retorna todos os pacientes cadastrados
+  patient.get("/:id") //autenticação Retonra o paciente expecifico por ID
+  patient.patch("/:id") //autenticação Atualiza o paciente expecifico por ID
+  patient.delete("/:id") //autenticação e adm Soft Delete do paciente por ID
 
   return patient
 }
