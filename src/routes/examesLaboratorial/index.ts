8237e21ccdeb12c5d660e58d4a05960c28eeec18
raqueldesa <silva.raquel.de.sa@gmail.com>
2022-09-11 13:28:25 -0400

    feat:update (by id) labExams

--- src/routes/examesLaboratorial/index.ts
+++ src/routes/examesLaboratorial/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { examLabCreateController } from "../../controllers/examesLaboratoriais/examLabCreate.controller";
 import { examLabDeleteController } from "../../controllers/examesLaboratoriais/examLabDelete.controller";
 import { examLabListOneByIdController } from "../../controllers/examesLaboratoriais/examLabListById.controller";
+import { examLabUpdateController } from "../../controllers/examesLaboratoriais/examLabUpdate.controller";
 import { examsLabListController } from "../../controllers/examesLaboratoriais/examsLabList.controller";
 
 const labExams = Router();
@@ -11,7 +12,7 @@ export const labExamsRouter = () => {
   labExams.post("/register", examLabCreateController);
   labExams.get("", examsLabListController);
   labExams.get("/paciente/:id", examLabListOneByIdController);
-  labExams.patch("/:id");
+  labExams.patch("/:id", examLabUpdateController);
   labExams.delete("/:id", examLabDeleteController);
   return labExams;
 };
