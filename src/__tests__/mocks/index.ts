c73b7846f1b60186ece16e97ede5aea5a61c91b2
lVladimirl <infinitdungeun@gmail.com>
2022-09-12 11:45:30 -0300

    feat/testConsultas

--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,70 +1,88 @@
-import { IMedicoLogin, IMedicoRequest, IMedicoUpdate } from "../../interfaces/medicos"
-import { IPacienteRequest, IPacienteUpdate } from "../../interfaces/pacientes"
+import { IConsultRequest } from "../../interfaces/consultas";
+import {
+  IMedicoLogin,
+  IMedicoRequest,
+  IMedicoUpdate,
+} from "../../interfaces/medicos";
+import { IPacienteRequest, IPacienteUpdate } from "../../interfaces/pacientes";
 
+export const medicoProfessor: IMedicoRequest = {
+  nome: "Kamila",
+  email: "kamila@gmail.com",
+  senha: "1234",
+  categoria: "R4",
+};
 
-export const medicoProfessor : IMedicoRequest = {
-    nome: "Kamila",
-    email: "kamila@gmail.com",
-    senha: "1234", 
-    categoria: "R4" 
-}
+export const medicoNormal: IMedicoRequest = {
+  nome: "Cayo",
+  email: "cayo@gmail.com",
+  senha: "1234",
+  categoria: "R2",
+};
 
-export const medicoNormal : IMedicoRequest = {
-    nome: "Cayo",
-    email: "cayo@gmail.com",
-    senha: "1234", 
-    categoria: "R2" 
-}
+export const medicoAtualizado: IMedicoUpdate = {
+  nome: "Ricardo",
+  email: "ricardo@gmail.com",
+  categoria: "Professor",
+};
 
-export const medicoAtualizado : IMedicoUpdate = {
-    nome: "Ricardo",
-    email: "ricardo@gmail.com",
-    categoria: "Professor"
-}
+export const medicoAtualizadoCategoriaAdm: IMedicoUpdate = {
+  categoria: "R4",
+};
 
-export const medicoAtualizadoCategoriaAdm : IMedicoUpdate = {
-    categoria: "R4"
-}
-
-export const medicoAtualizadoCategoriaNormal : IMedicoUpdate = {
-    categoria: "R2"
-}
+export const medicoAtualizadoCategoriaNormal: IMedicoUpdate = {
+  categoria: "R2",
+};
 
 export const loginMedicoNormal: IMedicoLogin = {
-    email: "cayo@gmail.com",
-    senha: "1234", 
-}
+  email: "cayo@gmail.com",
+  senha: "1234",
+};
 
 export const loginMedicoProfessor: IMedicoLogin = {
-    email: "kamila@gmail.com",
-    senha: "1234",
-}
+  email: "kamila@gmail.com",
+  senha: "1234",
+};
 
 export const loginMedico: IMedicoLogin = {
-    email: "emailErrado@gmail.com",
-    senha: "senhaForteMasNãoCorreta",
-}
+  email: "emailErrado@gmail.com",
+  senha: "senhaForteMasNãoCorreta",
+};
 
-export const patiente: IPacienteRequest ={
-    nome: "Matheus De Souza",
-    cpf: '123.456.789-10',
-    email: 'pedro@gmail.com',
-    dataNascimento: "01/01/2000",
-    cidadeOrigem: "Piracicaba", 
-    idade: 22,
-    nomeBebe: "Enzo da Silva",
-    nomePai: "Matheus De Souza",
-    diagnostico: "braço quebrado", 
-    procedimentos: "desquebrar o braço",
-    cariotipo: "banda g",
-}
+export const patiente: IPacienteRequest = {
+  nome: "Matheus De Souza",
+  cpf: "123.456.789-10",
+  email: "pedro@gmail.com",
+  dataNascimento: "01/01/2000",
+  cidadeOrigem: "Piracicaba",
+  idade: 22,
+  nomeBebe: "Enzo da Silva",
+  nomePai: "Matheus De Souza",
+  diagnostico: "braço quebrado",
+  procedimentos: "desquebrar o braço",
+  cariotipo: "banda g",
+};
 
 export const pacienteAtualizado: IPacienteUpdate = {
-    nome: "Lucas da Silva Neto",
-    email: "lucas.silva@gmail.com",
-    nomeBebe: "Fabio da Silva"
-}
+  nome: "Lucas da Silva Neto",
+  email: "lucas.silva@gmail.com",
+  nomeBebe: "Fabio da Silva",
+};
 
 export const pacienteAtualizadoSemPermissao: IPacienteUpdate = {
-    nome: "Abc Nome Não Autorizado"
-}
+  nome: "Abc Nome Não Autorizado",
+};
+
+export const consult: IConsultRequest = {
+  conduta: "TESTE de APOINTMENT",
+  idadeGestacional: 1,
+  peso: 2,
+  pressaoArterial: "doze por oito",
+  apresentacao: "paciente está bem",
+  batimentoCardFetal: "doze por oito",
+  edema: "inchaço leve",
+  movimentacaoFetal: true,
+  retorno: "10-12-2022",
+  toqueVaginal: "realizado",
+  uteroFita: "30 cm ",
+};
--- src/controllers/consultas/apointmentList.controller.ts
+++ src/controllers/consultas/apointmentList.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 
-import apointmentListService from "../../services/consultas/apointmentList.service";
+import {apointmentListService} from "../../services/consultas/apointmentList.service";
 
-const apointmentListController = async (req: Request, res: Response) => {
+export const apointmentListController = async (req: Request, res: Response) => {
   const newAppoint = await apointmentListService();
 
   return res.status(200).send(newAppoint);
 };
 
-export default apointmentListController;
