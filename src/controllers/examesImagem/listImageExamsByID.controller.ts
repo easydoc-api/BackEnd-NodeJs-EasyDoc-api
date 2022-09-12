1ee50315db83ea31b3712193d261b913cf4b3c0e
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-09-12 18:15:15 -0300

    fix: add id format validation on exams image controller

--- src/controllers/examesImagem/listImageExamsByID.controller.ts
+++ src/controllers/examesImagem/listImageExamsByID.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import { AppError } from "../../errors/AppError"
 import { listImageExamsByIdService } from "../../services/examesImagem/listImageExamsByID.service"
 
 export const listImageExamsByIdController = async (
@@ -6,6 +7,14 @@ export const listImageExamsByIdController = async (
   res: Response
 ) => {
   const { id } = req.params
+
+  const idValid = id.match(
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+  )
+
+  if (!idValid) {
+    throw new AppError("Id inválido", 422)
+  }
   const exam = await listImageExamsByIdService(id)
 
   return res.json(exam)
