6f0c198b6ae5c47d903b02d04263363c8fc2857b
arc4dex <pedroiago.bernardes@gmail.com>
2022-09-13 16:13:23 -0300

    fix:create char service

---
+++ src/services/prontuarios/chartCreate.service.ts
@@ -0,0 +1,7 @@
+import AppDataSource from "../../data-source";
+import { Prontuario } from "../../entities/prontuario.entity";
+import { AppError } from "../../errors/AppError";
+
+export const charCreateService = async () => {
+  
+}
