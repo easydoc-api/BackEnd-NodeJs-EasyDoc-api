609e9ed26bf30e9a85080d493d833163aa1efa1c
arc4dex <pedroiago.bernardes@gmail.com>
2022-09-14 17:50:08 -0300

    feat: creat inser exame lab chart

--- src/routes/prontuarios/index.ts
+++ src/routes/prontuarios/index.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { chartDeleteController } from "../../controllers/prontuarios/chartDelete.controller";
 import { chartInsertDoctorController} from "../../controllers/prontuarios/chartInsertDoctor.controller";
+import { chartInsertExameLabController } from "../../controllers/prontuarios/chartInsertExameLab.controller";
 import { allChartsListController } from "../../controllers/prontuarios/chartList.controller";
 import { chartListByIdController } from "../../controllers/prontuarios/chartListById.controller";
 import { chartUpdateController } from "../../controllers/prontuarios/chartUpdate.controller";
+import { authTokenMiddleware } from "../../middlewares/authToken.middleware";
 //importar middleware
 const prontuarios = Router();
 
 export const prontuarioRoutes = () => {
   prontuarios.post("/medicos/:id", chartInsertDoctorController)
+  prontuarios.post("/exames_laboratoriais/:id", chartInsertExameLabController)
   prontuarios.get("", allChartsListController); //autenticacao, adm/prof/R4
   prontuarios.get("/pacientes/:id", chartListByIdController); //autenticação
   prontuarios.get("/consultas/:palavra_chave"); //autenticação
