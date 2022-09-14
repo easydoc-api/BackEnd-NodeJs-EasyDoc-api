435e427d902a452e7df4b24796215e8818820877
lVladimirl <infinitdungeun@gmail.com>
2022-09-14 20:50:01 -0300

    feat/prodHeroku

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,54 +2,52 @@ import { DataSource } from "typeorm"
 import "dotenv/config"
 
 const AppDataSource =
-//  new DataSource(
-//   {
-//     type: "postgres",
+ new DataSource(
+  {
+    type: "postgres",
 
-//     url: process.env.DB_URL,
+    url: process.env.DATABASE_URL,
 
-//     ssl:
-//       process.env.NODE_ENV === "production"
-//         ? { rejectUnauthorized: false }
-//         : false,
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false,
 
-//     synchronize: false,
-//     logging: false,
+    synchronize: false,
+    logging: false,
 
-//     entities:
-//       process.env.NODE_ENV === "production"
-//         ? ["dist/src/entities/*.js"]
-//         : ["src/entities/*.ts"],
+    entities:
+      process.env.NODE_ENV === "production"
+        ? ["dist/src/entities/*.js"]
+        : ["src/entities/*.ts"],
 
-//     migrations:
-//       process.env.NODE_ENV === "production"
-//         ? ["dist/src/migrations/*.js"]
-//         : ["src/migrations/*.ts"],
-//   }
-//  )
+    migrations:
+      process.env.NODE_ENV === "production"
+        ? ["dist/src/migrations/*.js"]
+        : ["src/migrations/*.ts"],
+  }
+ )
 
-  //  QUANDO IMPLEMENTARMOS OS TESTES USAREMOS O MODELO ABAIXO:
+  // process.env.NODE_ENV === "test"
 
-  process.env.NODE_ENV === "test"
-
-  ? new DataSource({
-      type: "sqlite",
-      database: ":memory:",
-      entities: ["src/entities/*.ts"],
-      synchronize: true,
-    })
-  : new DataSource({
-      type: "postgres",
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB,
-      logging: true,
-      synchronize: false,
-      entities: ["src/entities/*.ts"],
-      migrations: ["src/migrations/*.ts"],
-    })
+  // ? new DataSource({
+  //     type: "sqlite",
+  //     database: ":memory:",
+  //     entities: ["src/entities/*.ts"],
+  //     synchronize: true,
+  //   })
+  // : new DataSource({
+  //     type: "postgres",
+  //     host: process.env.DB_HOST,
+  //     port: 5432,
+  //     username: process.env.DB_USER,
+  //     password: process.env.DB_PASSWORD,
+  //     database: process.env.DB,
+  //     logging: true,
+  //     synchronize: false,
+  //     entities: ["src/entities/*.ts"],
+  //     migrations: ["src/migrations/*.ts"],
+  //   })
 
 
 export default AppDataSource
